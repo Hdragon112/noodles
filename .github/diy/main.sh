42b55403431c45e9376c6fc061cdf37eb1678dcc
Hdragon112 <18706334023@163.com>
2026-05-03 14:30:37 +0800

    Update main.sh

--- .github/diy/main.sh
+++ .github/diy/main.sh
@@ -174,33 +174,33 @@ rm -rf mosdns && rm -rf luci-app-mosdns
 git clone --depth 1 -b v5 https://github.com/sbwml/luci-app-mosdns openwrt-mos && mv -n openwrt-mos/{*mosdns,v2dat} ./; rm -rf openwrt-mos
 git clone --depth 1 https://github.com/sbwml/v2ray-geodata
 
-git_sparse_clone master "https://github.com/coolsnowwolf/packages" "leanpkg" net/miniupnpd net/mwan3 multimedia/UnblockNeteaseMusic-Go \
-multimedia/UnblockNeteaseMusic net/amule net/antileech net/baidupcs-web net/frp multimedia/gmediarender net/go-aliyundrive-webdav net/msd_lite \
-net/qBittorrent-static net/qBittorrent net/transmission net/phtunnel libs/qtbase libs/qttools libs/rblibtorrent \
-net/uugamebooster net/verysync net/vlmcsd net/dnsforwarder net/nps net/tcpping net/netatalk net/pgyvpn net/n2n net/ntpclient net/gowebdav
-mv -f miniupnpd miniupnpd-iptables
-git_sparse_clone openwrt-25.12 "https://github.com/coolsnowwolf/luci" "leluci" applications libs/luci-lib-fs
-mv -f applications luciapp
-git_sparse_clone master "https://github.com/coolsnowwolf/lede" "leanlede" package/lean package/network/services/shellsync package/network/services/e2guardian
-mv -n lean/* luciapp/; rm -rf lean
-rm -rf luciapp/default-settings
-rm -rf luciapp/autocore
-rm -rf luciapp/automount
-rm -rf luciapp/ipv6-helper
+##git_sparse_clone master "https://github.com/coolsnowwolf/packages" "leanpkg" net/miniupnpd net/mwan3 multimedia/UnblockNeteaseMusic-Go \
+##multimedia/UnblockNeteaseMusic net/amule net/antileech net/baidupcs-web net/frp multimedia/gmediarender net/go-aliyundrive-webdav net/msd_lite \
+##net/qBittorrent-static net/qBittorrent net/transmission net/phtunnel libs/qtbase libs/qttools libs/rblibtorrent \
+##net/uugamebooster net/verysync net/vlmcsd net/dnsforwarder net/nps net/tcpping net/netatalk net/pgyvpn net/n2n net/ntpclient net/gowebdav
+##mv -f miniupnpd miniupnpd-iptables
+##git_sparse_clone openwrt-25.12 "https://github.com/coolsnowwolf/luci" "leluci" applications libs/luci-lib-fs
+##mv -f applications luciapp
+##git_sparse_clone master "https://github.com/coolsnowwolf/lede" "leanlede" package/lean package/network/services/shellsync package/network/services/e2guardian
+##mv -n lean/* luciapp/; rm -rf lean
+##rm -rf luciapp/default-settings
+##rm -rf luciapp/autocore
+##rm -rf luciapp/automount
+##rm -rf luciapp/ipv6-helper
 
-git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/packages" "immpkgs" net/mwol net/3proxy \
-net/sub-web net/dnsproxy net/haproxy net/v2raya net/cdnspeedtest net/keepalived net/dae net/modemband \
-net/subconverter net/ngrokc net/oscam net/njitclient net/scutclient net/gost  net/dufs \
-libs/jpcre2 libs/wxbase libs/rapidjson libs/libcron libs/quickjspp libs/toml11 \
-utils/cpulimit utils/filebrowser utils/cups net/udp2raw net/msd_lite net/xfrpc net/bitsrunlogin-go \
-utils/phicomm-k3screenctrl net/apinger net/basicstation libs/sx1302_hal sound/spotifyd
-git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/immortalwrt" "immortal" package/emortal/autocore package/emortal/automount \
-package/utils/mhz
-#package/network/utils/fullconenat package/network/utils/fullconenat-nft
-git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/luci" "immluci" applications protocols/luci-proto-minieap
-mv -n applications/* luciapp/; rm -rf applications
+##git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/packages" "immpkgs" net/mwol net/3proxy \
+##net/sub-web net/dnsproxy net/haproxy net/v2raya net/cdnspeedtest net/keepalived net/dae net/modemband \
+##net/subconverter net/ngrokc net/oscam net/njitclient net/scutclient net/gost  net/dufs \
+##libs/jpcre2 libs/wxbase libs/rapidjson libs/libcron libs/quickjspp libs/toml11 \
+##utils/cpulimit utils/filebrowser utils/cups net/udp2raw net/msd_lite net/xfrpc net/bitsrunlogin-go \
+##utils/phicomm-k3screenctrl net/apinger net/basicstation libs/sx1302_hal sound/spotifyd
+##git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/immortalwrt" "immortal" package/emortal/autocore package/emortal/automount \
+##package/utils/mhz
+###package/network/utils/fullconenat package/network/utils/fullconenat-nft
+##git_sparse_clone openwrt-25.12 "https://github.com/immortalwrt/luci" "immluci" applications protocols/luci-proto-minieap
+##mv -n applications/* luciapp/; rm -rf applications
 
-#git_sparse_clone openwrt-23.05 "https://github.com/openwrt/packages" "oppkgs1" net/shadowsocks-libev
+###git_sparse_clone openwrt-23.05 "https://github.com/openwrt/packages" "oppkgs1" net/shadowsocks-libev
 
 mv -n luciapp/* ./ ; rm -Rf luciapp
 #svn export https://github.com/coolsnowwolf/luci/trunk/libs/luci-lib-ipkg
@@ -221,9 +221,9 @@ git_sparse_clone develop "https://github.com/Ysurac/openmptcprouter-feeds" "enmp
 luci-app-packet-capture luci-app-mail msmtp
 git_sparse_clone master "https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng" "xiaoqingfeng" homeredirect luci-app-homeredirect
 
-git_sparse_clone master "https://github.com/x-wrt/packages" "x-wrtpack" net/nft-qos
-git_sparse_clone master "https://github.com/x-wrt/luci" "x-wrtluci" applications/luci-app-nft-qos
-git_sparse_clone master "https://github.com/x-wrt/com.x-wrt" "x-wrt" natflow lua-ipops luci-app-macvlan luci-app-3ginfo-lite luci-app-fakemesh urllogger
+##git_sparse_clone master "https://github.com/x-wrt/packages" "x-wrtpack" net/nft-qos
+##git_sparse_clone master "https://github.com/x-wrt/luci" "x-wrtluci" applications/luci-app-nft-qos
+##git_sparse_clone master "https://github.com/x-wrt/com.x-wrt" "x-wrt" natflow lua-ipops luci-app-macvlan luci-app-3ginfo-lite luci-app-fakemesh urllogger
 #git_sparse_clone master "https://github.com/immortalwrt/immortalwrt" "immortal" package/network/utils/nftables \
 
 #mv -n openwrt-passwall/* ./ ; rm -Rf openwrt-passwall
